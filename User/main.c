9157aa34163907df7456ade9b5bdd258d0324221
Lennox <corgon@post.sk>
2025-01-31 17:11:04 +0100

    Update main.c

--- User/main.c
+++ User/main.c
@@ -10,28 +10,29 @@
  * VDD 3.3V
  */
 
-#define PWM_STEPS 180 // Number of steps for the LED pulse effect
+#define PWM_STEPS 220 // Number of steps for the LED pulse effect
 
 uint16_t pwm_values[PWM_STEPS];
 
 void Generate_PWM_Values() {
-    for (int i = 0; i < PWM_STEPS / 2; i++) {
-        pwm_values[i] = (i * 1000) / (PWM_STEPS / 2);  // DEC LED PWM brightness
+    uint16_t step_size = 1023 / (PWM_STEPS / 2);
+    for (uint16_t i = 0; i < PWM_STEPS / 2; i++) {
+        pwm_values[i] = i * step_size;                 // DEC LED PWM brightness
         pwm_values[PWM_STEPS - i - 1] = pwm_values[i]; // INC LED PWM brightness
     }
 }
 
 void GPIO_init() {
     // GPIOA Setup
-    RCC->APB2PCENR |= RCC_IOPAEN | RCC_AFIOEN;  // Enable clocks for GPIOA and AFIO
-    AFIO->PCFR1 &= ~(1 << 15);                  // PA12_RM bit must be cleared when PA1 and PA2 are used as normal GPIO pins
+    RCC->APB2PCENR |= (1 << 2) | (1 << 0);      // Enable clocks for GPIOA and AFIO
+    AFIO->PCFR1   &= ~(1 << 15);                // PA12_RM bit must be cleared when PA1 and PA2 are used as normal GPIO pins
 
     GPIOA->CFGLR &= ~(0xFF << 4);               // Reset configuration for pins PA1 and PA2
     GPIOA->CFGLR |=  (0x88 << 4);               // Set mode to Input-PullUp
     GPIOA->OUTDR |=  (3 << 1);                  // Activate pull-up resistors for PA1 and PA2
 
     // GPIOC Setup
-    RCC->APB2PCENR |= RCC_IOPCEN;               // Enable clock for GPIOC
+    RCC->APB2PCENR |=(1 << 4);                  // Enable clock for GPIOC
     GPIOC->CFGLR &= ~(0xF << 16);               // Clear previous settings
     GPIOC->CFGLR |=  (0xB << 16);               // Alternate function PC4 push-pull 10MHz
 
@@ -40,39 +41,38 @@ void GPIO_init() {
 }
 
 void TIM1_PWMOut() {
-    RCC->APB2PCENR |= RCC_APB2Periph_TIM1;      // Enable clock for TIM1
+    RCC->APB2PCENR |= (1 << 11);                // Enable clock for TIM1
     TIM1->PSC   = 480 - 1;                      // 48 MHz
-    TIM1->ATRLR = 1000 - 1;                     // 1000 Hz / 1000 = 1 Hz
+    TIM1->ATRLR = 1000 - 1;                     // 1000 Hz / 10 = 100 Hz
 
-    // Set PWM mode for CH4
     TIM1->CHCTLR2 &= ~(7 << 12);                // Clear OC4M bits
     TIM1->CHCTLR2 |= (7 << 12);                 // PWM mode 2
     TIM1->CHCTLR2 |= (1 << 11);                 // Enable preload mode
     TIM1->CCER    |= (1 << 12);                 // CC4E = 1 (Enable TIM1 CH4)
     TIM1->BDTR    |= (1 << 15);                 // Enable main output (MOE)
     TIM1->CTLR1   |= (1 << 7);                  // ARPE (Auto-reload preload enable)
-    TIM1->CTLR1   |= TIM_CEN;                   // Start the TIM1 timer
+    TIM1->CTLR1   |= (1 << 0);                  // Start the TIM1 timer
 
-    // DMA configuration for CH4CVR (DBL max = 31)
-    TIM1->DMACFGR = ((PWM_STEPS > 32 ? 31 : PWM_STEPS - 1) << 8) | (0x3C);
-    TIM1->DMAINTENR |= (1 << 12);               // Enable DMA for CC4
+    TIM1->DMACFGR = ((PWM_STEPS > 32 ? 31 :
+                     PWM_STEPS - 1) << 8) | (0x3C); // DMA configuration for CH4CVR (DBL max = 31)
+    TIM1->DMAINTENR |= (1 << 12);                   // Enable DMA for CC4
 }
 
 void DMA_Config(void) {
-    RCC->AHBPCENR |= RCC_AHBPeriph_DMA1;                     // Enable clock for DMA1
+    RCC->AHBPCENR |= (1 << 0);                      // Enable clock for DMA1
 
-    DMA1_Channel4->PADDR = (uint32_t)&TIM1->CH4CVR;          // Peripheral address (TIM1 CH4CVR register) for DMA
-    DMA1_Channel4->MADDR = (uint32_t)pwm_values;             // Memory address where PWM values are stored
-    DMA1_Channel4->CNTR  = PWM_STEPS;                        // SNumber of data items to transfer (PWM_STEPS)
+    DMA1_Channel4->PADDR = (uint32_t)&TIM1->CH4CVR; // Peripheral address (TIM1 CH4CVR register) for DMA
+    DMA1_Channel4->MADDR = (uint32_t)pwm_values;    // Memory address where PWM values are stored
+    DMA1_Channel4->CNTR  = PWM_STEPS;               // SNumber of data items to transfer (PWM_STEPS)
 
-    DMA1_Channel4->CFGR  = DMA_MemoryInc_Enable |            // Enable memory increment (DMA will increment memory address after each transfer)
-                           DMA_Mode_Circular |               // Enable circular mode (DMA will keep transferring data in a loop)
-                           DMA_PeripheralDataSize_HalfWord | // Set data size for peripheral to 16-bit (HalfWord)
-                           DMA_MemoryDataSize_HalfWord |     // Set data size for memory to 16-bit (HalfWord)
-                           DMA_DIR_PeripheralDST |           // Set transfer direction: memory to peripheral (Peripheral Destination)
-                           DMA_Priority_High;                // Set high priority for DMA transfer
+    DMA1_Channel4->CFGR = (1 << 7)  |               // Memory increment enable (MINC)
+                          (1 << 5)  |               // Circular mode enable (CIRC)
+                          (1 << 8)  |               // Peripheral data size = 16-bit (PSIZE[0:1])
+                          (1 << 10) |               // Memory data size = 16-bit (MSIZE[0:1])
+                          (1 << 4)  |               // Data transfer direction: Memory to Peripheral DIR = 1
+                          (0 << 12);                // Priority level = Low (PL[0:0])
 
-    DMA1_Channel4->CFGR |= (1 << 0);                         // Enable DMA
+    DMA1_Channel4->CFGR |= (1 << 0);                // Enable DMA
 }
 
 int main(void) {
@@ -83,14 +83,7 @@ int main(void) {
     Generate_PWM_Values();
 
     while (1) {
-        (GPIOA->INDR & (GPIO_INDR_IDR1)) ? (GPIOC->BSHR |= (GPIO_BSHR_BR2)) : (GPIOC->BSHR |= (GPIO_BSHR_BS2)); // PA1 = On LED C2
-        (GPIOA->INDR & (GPIO_INDR_IDR2)) ? (GPIOC->BSHR |= (GPIO_BSHR_BR1)) : (GPIOC->BSHR |= (GPIO_BSHR_BS1)); // PA2 = On LED C1
-    }
+    (GPIOA->INDR & (1 << 1)) ? (GPIOC->BSHR |= (1 << 17)) : (GPIOC->BSHR |= (1 << 1)); // PA1 = On LED C1
+    (GPIOA->INDR & (1 << 2)) ? (GPIOC->BSHR |= (1 << 18)) : (GPIOC->BSHR |= (1 << 2)); // PA2 = On LED C2
+        }
 }
-
-
-
-
-
-
-
